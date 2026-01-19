663b925e84f64a2d4dd19ecbb5334d64835d2b2f
DangQuangTien <dangquangtien16102002@gmail.com>
2026-01-19 23:31:25 +0700

    update return more Data back to FE

--- DAL/Repositories/AssignmentRepository.cs
+++ DAL/Repositories/AssignmentRepository.cs
@@ -6,35 +6,47 @@ namespace DAL.Repositories
 {
     public class AssignmentRepository : Repository<Assignment>, IAssignmentRepository
     {
-        public AssignmentRepository(ApplicationDbContext context) : base(context) { }
+        private readonly ApplicationDbContext _context;
+
+        public AssignmentRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
 
         public async Task<List<Assignment>> GetAssignmentsByAnnotatorAsync(int projectId, string annotatorId)
         {
-            return await _dbSet
-                .Include(a => a.DataItem)
-                .Where(a => a.AnnotatorId == annotatorId && (projectId == 0 || a.ProjectId == projectId))
-                .OrderByDescending(a => a.AssignedDate)
-                .ToListAsync();
+            var query = _context.Assignments
+                .Include(a => a.DataItem) 
+                .Include(a => a.Project)
+                .Where(a => a.AnnotatorId == annotatorId);
+
+            if (projectId > 0)
+            {
+                query = query.Where(a => a.ProjectId == projectId);
+            }
+
+            return await query.OrderByDescending(a => a.AssignedDate).ToListAsync();
         }
 
         public async Task<List<Assignment>> GetAssignmentsForReviewerAsync(int projectId)
         {
-            return await _dbSet
-                .Include(a => a.DataItem)
-                .Include(a => a.Annotator)
+            return await _context.Assignments
+                .Include(a => a.DataItem)   
+                .Include(a => a.Annotator)  
                 .Where(a => a.ProjectId == projectId && a.Status == "Submitted")
+                .OrderBy(a => a.SubmittedAt)
                 .ToListAsync();
         }
 
-        public async Task<Assignment?> GetAssignmentWithDetailsAsync(int id)
+        public async Task<Assignment?> GetAssignmentWithDetailsAsync(int assignmentId)
         {
-            return await _dbSet
+            return await _context.Assignments
                 .Include(a => a.DataItem)
                 .Include(a => a.Project)
                     .ThenInclude(p => p.LabelClasses)
                 .Include(a => a.Annotations)
                 .Include(a => a.ReviewLogs)
-                .FirstOrDefaultAsync(a => a.Id == id);
+                .FirstOrDefaultAsync(a => a.Id == assignmentId);
         }
         public async Task<List<DataItem>> GetUnassignedDataItemsAsync(int projectId, int quantity)
         {
